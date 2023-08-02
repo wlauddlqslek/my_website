6c9db0e31971cc3c408540b8bd3208e4023796a7
wlauddlqslek <wlaud070510@gmail.com>
2023-08-03 02:14:12 +0900

    한국어 추가

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 window.addEventListener('load', () => {
     const $s = {
         input: document.getElementById('input'),
+        kor: document.getElementById('kor'),
+        noKor: document.getElementById('noKor'),
         eng: document.getElementById('eng'),
         noEng: document.getElementById('noEng')
-    }
+    };
+
+    const korRE = /^[ㄱ-ㅎㅏ-ㅣ가-힣0-9]+$/i;
+    const engRE = /^[a-z0-9\s]+$/i;
+
 
     $s.input.onkeyup = () => {
-        const arr = $s.input.value.split('\n')
+        const arr = $s.input.value.split('\n');
+        const korArr = arr.filter(a => {
+            return korRE.test(a)
+        });
+        const noKorArr = arr.filter(a => {
+            return !(korRE.test(a))
+        });
         const engArr = arr.filter(a => {
-            return /^[a-z0-9\s]+$/i.test(a)
-        })
+            return engRE.test(a)
+        });
         const noEngArr = arr.filter(a => {
-            return !(/^[a-z0-9\s]+$/i.test(a))
-        })
-        $s.eng.value = engArr.join('\n')
-        $s.noEng.value = noEngArr.join('\n')
-    }
-})
+            return !(engRE.test(a))
+        });
+        $s.kor.value = korArr.join('\n');
+        $s.noKor.value = noKorArr.join('\n');
+        $s.eng.value = engArr.join('\n');
+        $s.noEng.value = noEngArr.join('\n');
+    };
+});
